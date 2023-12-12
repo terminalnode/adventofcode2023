116d6399ba38a8ebc1605634e626cb357e627385
Alexander Rundberg <alex@sect.tech>
2023-12-12 10:15:43 +0100

    day 12 initial support for copies

--- src/solution/day12.rs
+++ src/solution/day12.rs
@@ -1,79 +1,153 @@
 use std::collections::VecDeque;
+use rayon::prelude::*;
 use crate::solution::Solution;
 
 pub struct Day12 {
 	file: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Record {
-	original_row: usize,
 	springs: Vec<char>,
 	groups: Vec<usize>,
 }
 
+#[derive(Debug)]
+struct RecordProgress {
+	record: Record,
+	broke_in_group: usize,
+	group_index: usize,
+}
+
 impl Day12 {
-	fn parse(&self) -> Result<Vec<Record>, String> {
+	fn parse(
+		&self,
+		copies: usize,
+	) -> Result<Vec<Record>, String> {
 		let file = self.read_file_as_string()?;
-		let mut records = Vec::new();
-		for (i, line) in file.lines().enumerate() {
+		let lines = file.lines().collect::<Vec<&str>>();
+
+		let records = lines.par_iter().enumerate().filter_map(|(i, line)| {
 			let mut split = line.split(" ");
-			let springs: Vec<char> = split.next()
-				.ok_or(format!("Invalid line: {}", line))?
-				.chars()
-				.collect();
+			let springs: Vec<char> = split.next()?.chars().collect();
 
-			let group_split = split.next().ok_or(format!("Invalid line: {}", line))?;
-			let groups: Vec<usize> = group_split.split(",").map(|s| s.parse::<usize>().unwrap()).collect();
+			let group_split = split.next()?;
+			let groups: Vec<usize> = group_split.split(",")
+				.map(|s| s.parse::<usize>().unwrap())
+				.collect::<Vec<usize>>();
 
-			records.extend(expand(i, &springs, &groups));
-		}
+			let x = Some(expand(&springs.repeat(copies), &groups.repeat(copies)));
+			println!("Finished {i}");
+			x
+		}).flatten().collect::<Vec<Record>>();
 
+		println!("Finished parsing");
 		Ok(records)
 	}
 }
 
+fn as_readable(rp: &RecordProgress) -> String {
+	rp.record.springs.iter().collect::<String>()
+}
+
 fn expand(
-	original_row: usize,
 	springs: &Vec<char>,
 	groups: &Vec<usize>,
 ) -> Vec<Record> {
-	let mut expanded_springs: Vec<Vec<char>> = vec![vec![]];
+	let readable = springs.iter().collect::<String>();
+	let group_count = groups.len();
+	let mut progresses: Vec<RecordProgress> = vec![
+		RecordProgress {
+			record: Record {
+				springs: vec![],
+				groups: groups.clone(),
+			},
+			broke_in_group: 0,
+			group_index: 0,
+		},
+	];
 
-	for (i, c) in springs.iter().enumerate() {
-		match c {
-			'.' | '#' => {
-				for ex_spring in &mut expanded_springs {
-					ex_spring.push(*c)
-				}
-			}
-
+	for (i, char) in springs.iter().enumerate() {
+		progresses = match char {
 			'?' => {
-				let new_springs: Vec<Vec<char>> = expanded_springs.iter()
-					.map(|s| {
-						let mut clone = s.clone();
-						clone.push('#');
-						clone
-					}).collect();
-				for ex_spring in &mut expanded_springs {
-					ex_spring.push('.')
-				}
-				expanded_springs.extend(new_springs);
+				progresses.iter().flat_map(|p| {
+					let next_group_count = groups.get(p.group_index);
+					let mut new: Vec<RecordProgress> = vec![];
+
+					if next_group_count != None && next_group_count > Some(&p.broke_in_group) {
+						let mut record = p.record.clone();
+						record.springs.push('#');
+						new.push(RecordProgress {
+							record,
+							broke_in_group: p.broke_in_group + 1,
+							group_index: p.group_index,
+						});
+					}
+
+					// If not in a group, or group has right size, add dot
+					if p.broke_in_group == 0 || next_group_count == Some(&p.broke_in_group) {
+						let mut record = p.record.clone();
+						record.springs.push('.');
+						new.push(RecordProgress {
+							record,
+							broke_in_group: 0,
+							group_index: if p.broke_in_group > 0 { p.group_index + 1 } else { p.group_index },
+						});
+					}
+
+					new
+				}).collect()
 			}
-			_ => panic!("whaaaat")
-		}
+
+			'.' => {
+				progresses.retain_mut(|p| {
+					let next_group_count = groups.get(p.group_index);
+					if p.broke_in_group != 0 && next_group_count == None {
+						return false;
+					} else if p.broke_in_group != 0 && Some(&p.broke_in_group) != next_group_count {
+						return false;
+					}
+					p.record.springs.push('.');
+
+					if p.broke_in_group != 0 {
+						p.broke_in_group = 0;
+						p.group_index += 1;
+					}
+
+					true
+				});
+				progresses
+			}
+
+			'#' => {
+				progresses.retain_mut(|p| {
+					let next_group_count = groups.get(p.group_index);
+					p.broke_in_group += 1;
+
+					if p.broke_in_group == 0 && next_group_count == None {
+						return false;
+					} else if Some(&p.broke_in_group) > next_group_count {
+						return false;
+					}
+					p.record.springs.push('#');
+
+					true
+				});
+				progresses
+			}
+
+			c => panic!("Unknown char '{c}' in {springs:?}")
+		};
 	}
 
-	expanded_springs.iter().map(|springs| {
-		Record {
-			original_row,
-			springs: springs.clone(),
-			groups: groups.clone(),
-		}
-	}).collect()
+	progresses.retain(|rp| {
+		validate(&rp.record, true)
+	});
+
+	progresses.iter().map(|rp| rp.record.clone()).collect()
 }
 
-fn validate(record: &Record) -> bool {
+fn validate(record: &Record, is_final: bool) -> bool {
 	let mut groups = VecDeque::from(record.groups.clone());
 	let mut current: usize = 0;
 	let mut next = groups.pop_front();
@@ -81,7 +155,7 @@ fn validate(record: &Record) -> bool {
 	for (i, c) in record.springs.iter().enumerate() {
 		match c {
 			'.' => {
-				if current != 0 && next != Some(current) {
+				if is_final && current != 0 && next != Some(current) {
 					return false;
 				} else if current != 0 {
 					next = groups.pop_front();
@@ -93,7 +167,6 @@ fn validate(record: &Record) -> bool {
 				if current == 0 && next == None {
 					return false;
 				} else if next <= Some(current) {
-					// println!("next({next:?}) <= Some(current) @ {i}");
 					return false;
 				} else {
 					current += 1;
@@ -104,7 +177,9 @@ fn validate(record: &Record) -> bool {
 		};
 	}
 
-	return if current != 0 && Some(current) != next {
+	return if !is_final {
+		true
+	} else if current != 0 && Some(current) != next {
 		false
 	} else if next > Some(current) {
 		false
@@ -120,9 +195,15 @@ impl Solution for Day12 {
 	fn get_file_name(&self) -> &str { &self.file }
 
 	fn part_one(&self) -> Result<String, String> {
-		Ok(self.parse()?
-			.iter()
-			.filter(|&r| validate(r))
+		Ok(self.parse(1)?
+			.par_iter()
+			.count()
+			.to_string())
+	}
+
+	fn part_two(&self) -> Result<String, String> {
+		Ok(self.parse(5)?
+			.par_iter()
 			.count()
 			.to_string())
 	}
